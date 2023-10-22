9762113547b61548fb5af06258c07c0f6adb09ea
Alessio Caputo <84250128+alessioC42@users.noreply.github.com>
2023-10-22 16:24:43 +0200

    fix navbar user

--- static/js/navbar_login_manager.js
+++ static/js/navbar_login_manager.js
@@ -1,30 +1,33 @@
-let navbarUserLoggedIn = document.getElementById("navbar-user-logged-in");
-let navbarUserNotLoggedIn = document.getElementById("navbar-user-not-logged-in");
+(()=>{
+    let navbarUserLoggedIn = document.getElementById("navbar-user-logged-in");
+    let navbarUserNotLoggedIn = document.getElementById("navbar-user-not-logged-in");
 
-let userID = localStorage.getItem("userID");
+    let userID = localStorage.getItem("userID");
 
-if (userID) {
-    navbarUserLoggedIn.classList.remove("visually-hidden");
-    navbarUserLoggedIn.style.cursor = "pointer";
-    navbarUserNotLoggedIn.classList.add("visually-hidden");
+    if (userID) {
+        navbarUserLoggedIn.classList.remove("visually-hidden");
+        navbarUserLoggedIn.style.cursor = "pointer";
+        navbarUserNotLoggedIn.classList.add("visually-hidden");
 
-    document.getElementById("navbar-user-click-to-profile").addEventListener("click", () => {
-        window.location.href="/profile?id="+userID;
-    });
+        document.getElementById("navbar-user-click-to-profile").addEventListener("click", () => {
+            window.location.href="/profile?id="+userID;
+        });
 
-    document.getElementById("navbar-button-logout").addEventListener("click", () => {
-        localStorage.clear();
-        window.location.href="/";
-    });
-    (async () => {
-        let response = await fetch("/api/user/"+userID);
-        let userData = await response.json();
+        document.getElementById("navbar-button-logout").addEventListener("click", () => {
+            localStorage.clear();
+            window.location.href="/";
+        });
+        (async () => {
+            let response = await fetch("/api/user/"+userID);
+            let userData = await response.json();
 
-        document.getElementById("navbar-user-name").innerText = userData.username;
+            document.getElementById("navbar-user-name").innerText = userData.username;
 
-    })();
+        })();
 
-} else {
-    navbarUserLoggedIn.classList.add("visually-hidden");
-    navbarUserNotLoggedIn.classList.remove("visually-hidden");
-}
+    } else {
+        navbarUserLoggedIn.classList.add("visually-hidden");
+        navbarUserNotLoggedIn.classList.remove("visually-hidden");
+    }
+
+})();
